19339860af97c574bc4b0aec08920d0ea06804b2
shadowDesktop <674805880@qq.com>
2023-07-27 20:52:29 +0800

    before lighj

--- Assets/Scripts/TestCla.cs
+++ Assets/Scripts/TestCla.cs
@@ -1,13 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class TestCla : MonoBehaviour
 {
+    public GameObject createObj;
+
     private void Start()
     {
         print(transform.FindFromAllChildren("Target").name);
         print(transform.FindFromAllChildren("Target2").name);
         print(transform.FindFromAllChildren("target3").name);       
+    }
+
+    private void Update()
+    {
+        ClickToCreate();
+    }
+
+    private void ClickToCreate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (createObj == null)
+                createObj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            Vector3 screenPos = Input.mousePosition;
+            screenPos.z = 20;
+            Vector3 pos = Camera.main.ScreenToWorldPoint(screenPos);
+
+            Instantiate(createObj, pos, transform.rotation);
+        }
     }
 }
